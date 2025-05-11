c2965e6ac5ac328757568779639a85e1f1d82781
DeinJoni <jonas.a.heinz@gmail.com>
2025-05-11 11:28:24 +0300

    add lp init

---
+++ lp/lp/app/layout.tsx
@@ -0,0 +1,28 @@
+import type React from "react"
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import type { Metadata } from "next"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Reka.finance",
+  description: "Create your own yield bearing DCA Flows and watch them grow in real time.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} min-h-screen flex items-center justify-center`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
